d3365b99dfccc2ad748da27d69d73285d267239b
jokokoloko <jhowland82@gmail.com>
2019-09-13 01:12:36 -0700

    Added @zeit/next-css

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
-module.exports = {
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
-    }
+const withCSS = require('@zeit/next-css');
 
-    return config
-  }
-}
+module.exports = withCSS({
+    webpack: (config) => {
+        // Fixes npm packages that depend on `fs` module
+        config.node = {
+            fs: 'empty',
+        };
+        return config;
+    },
+});
