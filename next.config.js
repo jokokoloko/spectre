9a65f949479a9431f5016f8d315148a0960334ed
jokokoloko <jhowland82@gmail.com>
2019-09-14 01:30:19 -0700

    Added @zeit/next-sass and node-sass

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const withCSS = require('@zeit/next-css');
+const withSass = require('@zeit/next-sass');
 
-module.exports = withCSS({
-    webpack: (config) => {
-        // Fixes npm packages that depend on `fs` module
-        config.node = {
-            fs: 'empty',
-        };
-        return config;
-    },
-});
+module.exports = withCSS(
+    withSass({
+        webpack: (config) => {
+            // Fixes npm packages that depend on `fs` module
+            config.node = {
+                fs: 'empty',
+            };
+            return config;
+        },
+    }),
+);
