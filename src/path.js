56edb7c32b64f210cce16d3bc8eb8c178e0c32ab
jokokoloko <jhowland82@gmail.com>
2019-09-14 00:33:47 -0700

    Added path.js

---
+++ src/path.js
@@ -0,0 +1,9 @@
+// Main
+export const ROOT = '/';
+export const POST = '/post';
+export const ABOUT = '/about';
+export const OVERVIEW = '/overview';
+
+// External
+export const GOOGLE = 'https://www.google.com/';
+export const YAHOO = 'https://www.yahoo.com/';
