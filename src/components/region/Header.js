326e542e6593c4d706ca1319018a05f1d82a02df
jokokoloko <jhowland82@gmail.com>
2019-09-14 22:19:26 -0700

    Updated Header.js

--- src/components/region/Header.js
+++ src/components/region/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import Link from 'next/link';
 import content from '../../content';
-import * as path from '../../path';
+import Link from '../unit/Link';
 import Logo from '../unit/Logo';
 
 const Header = () => {
@@ -11,10 +10,8 @@ const Header = () => {
     return (
         <header id="header" className={`navbar navbar-expand-lg navbar-${type}-top ${type}-top`} role="banner">
             <div className={container}>
-                <Link href={path.ROOT}>
-                    <a className="navbar-brand" title={title} rel="home">
-                        <Logo alternate={title} />
-                    </a>
+                <Link className="navbar-brand" title={title} rel="home">
+                    <Logo alternate={title} />
                 </Link>
                 <button type="button" id="menu-button" className="navbar-menu navbar-toggler" aria-label="Menu" aria-controls="menu-offcanvas">
                     <span className="icon-text sr-only">Menu</span>
