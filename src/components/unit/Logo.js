0152f7fe031f71412ce2391281ff99d8da803d9f
jokokoloko <jhowland82@gmail.com>
2019-09-15 05:38:18 -0700

    Added logo.js

--- src/components/unit/Logo.js
+++ src/components/unit/Logo.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import logo from '../../queries/logo';
 
-const Logo = ({ className, alternate }) => <img className={`logo img-fluid ${className}`} src="/static/favicon.ico" alt={alternate} />;
+const Logo = ({ className, alternate }) => <img className={`logo img-fluid ${className}`} src={logo()} alt={alternate} />;
 
 Logo.propTypes = {
     className: PropTypes.string,
