efd546592ade9d8d1053914ce6f8d79b24469320
jokokoloko <jhowland82@gmail.com>
2019-09-15 00:01:37 -0700

    Updated pages

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
-import Layout from '../src/components/Layout';
 import page from '../src/queries/page';
+import Layout from '../src/components/Layout';
+import Hero from '../src/components/section/Hero';
 
 export default () => {
     const { home } = page();
     return (
         <Layout>
-            <h1>{home.hero.headline}</h1>
+            {home.hero && (
+                <Hero id={home.hero.slug} height={home.hero.height} color={1}>
+                    <header>
+                        <h1>{home.hero.headline}</h1>
+                    </header>
+                </Hero>
+            )}
         </Layout>
     );
 };
--- pages/overview.js
+++ pages/overview.js
@@ -1,11 +1,17 @@
-import Layout from '../src/components/Layout';
 import page from '../src/queries/page';
+import Layout from '../src/components/Layout';
+import Basic from '../src/components/section/Basic';
 
 export default () => {
     const { overview } = page();
     return (
         <Layout>
-            <h1>{overview.hero.headline}</h1>
+            <Basic id="basic-overview">
+                <header className="copy node-xs-50 node-lg-80 text-lg-center">
+                    <h1>{overview.title}</h1>
+                    <h2>{overview.description}</h2>
+                </header>
+            </Basic>
         </Layout>
     );
 };
