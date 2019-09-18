fa9cfe7aef7551a3bab327dbb65ca1ad9a3947bd
jokokoloko <jhowland82@gmail.com>
2019-09-18 00:50:19 -0700

    Updated SEO.js

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -16,24 +16,30 @@ const SEO = ({ template, title: pageTitle, description: pageDescription, url, og
                 <body id="body" className={template} />
             </Helmet>
             <NextHead>
-                <meta charSet="utf-8" />
                 <title>{pageTitle ? `${pageTitle} - ${title}` : title}</title>
-                <meta name="description" content={metaDescription} />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" sizes="192x192" href="/static/touch-icon.png" />
                 <link rel="apple-touch-icon" href="/static/touch-icon.png" />
                 <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
                 <link rel="icon" href="/static/favicon.ico" />
+                <link rel="canonical" href={url || defaultOGURL} />
+                <meta charSet="utf-8" />
+                <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+                <meta name="description" content={metaDescription} />
                 <meta property="og:url" content={url || defaultOGURL} />
+                <meta property="og:site_name" content={title} />
                 {pageTitle && <meta property="og:title" content={pageTitle} />}
                 <meta property="og:description" content={metaDescription} />
+                <meta property="og:type" content={template.includes('single') ? 'article' : 'website'} />
+                {false && <meta property="og:image" content={ogImage || defaultOGImage} />}
+                {false && <meta property="og:image:width" content="1200" />}
+                {false && <meta property="og:image:height" content="630" />}
                 <meta name="twitter:site" content={url || defaultOGURL} />
+                <meta name="twitter:creator" content={title} />
                 {pageTitle && <meta name="twitter:title" content={pageTitle} />}
+                <meta name="twitter:description" content={metaDescription} />
                 <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:image" content={ogImage || defaultOGImage} />
-                <meta property="og:image" content={ogImage || defaultOGImage} />
-                <meta property="og:image:width" content="1200" />
-                <meta property="og:image:height" content="630" />
+                {false && <meta name="twitter:image" content={ogImage || defaultOGImage} />}
             </NextHead>
         </Fragment>
     );
