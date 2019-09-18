fccf27255ab0a110a848418a21be76b2107c7aa2
jokokoloko <jhowland82@gmail.com>
2019-09-17 19:01:54 -0700

    Updated Layout.js for Scroll

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import { Events } from 'react-scroll';
 import PropTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/function.scss';
@@ -7,15 +8,26 @@ import '../styles/project.scss';
 import SEO from './SEO';
 import Header from './region/Header';
 import Footer from './region/Footer';
+import Scroll from './widget/Scroll';
 
 class Layout extends Component {
     constructor(props) {
         super(props);
         this.state = {
             isOpen: false,
+            showScroll: false,
         };
         this.onOpen = this.onOpen.bind(this);
         this.onClose = this.onClose.bind(this);
+        this.onScroll = this.onScroll.bind(this);
+    }
+    componentDidMount() {
+        window.addEventListener('scroll', this.onScroll);
+        Events.scrollEvent.register('end', () => this.onClose());
+    }
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.onScroll);
+        Events.scrollEvent.remove('end');
     }
     onOpen() {
         this.setState({
@@ -27,9 +39,24 @@ class Layout extends Component {
             isOpen: false,
         });
     }
+    onScroll() {
+        const { showScroll } = this.state;
+        const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+        const offset = 210;
+        showScroll &&
+            scrollTop <= offset &&
+            this.setState({
+                showScroll: false,
+            });
+        !showScroll &&
+            scrollTop >= offset &&
+            this.setState({
+                showScroll: true,
+            });
+    }
     render() {
         const { template, title, description, children } = this.props;
-        const { isOpen } = this.state;
+        const { isOpen, showScroll } = this.state;
         const offcanvasPush = isOpen ? 'offcanvas-push offcanvas-push-out' : 'offcanvas-push';
         const style = {
             overlay: {
@@ -48,6 +75,7 @@ class Layout extends Component {
                     <div className="container-fluid">{children}</div>
                 </main>
                 <Footer offcanvasPush={offcanvasPush} />
+                {showScroll && <Scroll position="fixed" up top />}
             </Fragment>
         );
     }
