ef58d61cb8e05d9461cad4448824f4ae7cf788c5
jokokoloko <jhowland82@gmail.com>
2019-09-19 00:52:11 -0700

    Modified Link.js for ActiveLink

--- src/components/unit/Link.js
+++ src/components/unit/Link.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { Children } from 'react';
 import PropTypes from 'prop-types';
 import NextLink from 'next/link';
+import { useRouter } from 'next/router';
 import * as path from '../../path';
 
-const Link = ({ className, title, rel, to, children }) => (
-    <NextLink href={to}>
+const ActiveLink = ({ activeClassName, children, ...props }) => {
+    const { pathname } = useRouter();
+    const child = Children.only(children);
+    const className = pathname === props.href ? `${child.props.className} ${activeClassName}` : child.props.className;
+    return <NextLink {...props}>{React.cloneElement(child, { className })}</NextLink>;
+};
+
+ActiveLink.propTypes = {
+    activeClassName: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
+};
+
+const Link = ({ className, activeClassName, title, rel, to, children }) => (
+    <ActiveLink activeClassName={activeClassName} href={to}>
         <a className={className} title={title} rel={rel}>
             {children}
         </a>
-    </NextLink>
+    </ActiveLink>
 );
 
 Link.propTypes = {
     className: PropTypes.string,
+    activeClassName: PropTypes.string,
     title: PropTypes.string,
     rel: PropTypes.string,
     to: PropTypes.string,
@@ -21,6 +35,7 @@ Link.propTypes = {
 
 Link.defaultProps = {
     className: 'no-class',
+    activeClassName: 'active',
     title: undefined,
     rel: undefined,
     to: path.ROOT,
