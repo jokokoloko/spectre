ed2084442a24cfa6cef0fd614b0b7c612f2dd234
jokokoloko <jhowland82@gmail.com>
2019-09-29 02:56:33 -0700

    Updated index.js

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,13 @@ import Layout from '../src/components/Layout';
 import Hero from '../src/components/section/Hero';
 
 export default () => {
-    const { home } = usePage();
+    const { home: page } = usePage();
     return (
         <Layout template="home">
-            {home.hero && (
-                <Hero id={home.hero.slug} height={home.hero.height} color={1}>
+            {page.hero && (
+                <Hero id={page.hero.slug} height={page.hero.height} color={1}>
                     <header>
-                        <h1>{home.hero.headline}</h1>
+                        <h1>{page.hero.headline}</h1>
                     </header>
                 </Hero>
             )}
