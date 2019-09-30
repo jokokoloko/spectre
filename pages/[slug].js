7b499b9c6876e26c49dce55dc72b018781096bc2
jokokoloko <jhowland82@gmail.com>
2019-09-30 03:36:56 -0700

    Added [slug].js for Simple

---
+++ pages/[slug].js
@@ -0,0 +1,22 @@
+import usePage from '../src/queries/usePage';
+import Layout from '../src/components/Layout';
+import Basic from '../src/components/section/Basic';
+
+const Simple = ({ data: simple, slug }) => (
+    <Layout template={`single single-simple single-simple-${slug}`} title={simple.title} description={simple.description}>
+        <Basic id={slug} space="space-custom">
+            <header className="node-xs-30 node-lg-50">
+                <h1>{simple.title}</h1>
+            </header>
+            <section className="node-xs-30 node-lg-50" dangerouslySetInnerHTML={{ __html: simple.body }} />
+        </Basic>
+    </Layout>
+);
+
+Simple.getInitialProps = async ({ query }) => {
+    const { slug } = query;
+    const { [slug]: data } = usePage();
+    return { data, slug };
+};
+
+export default Simple;
