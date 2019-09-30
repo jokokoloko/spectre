873948b0772e1709c0ca8fd5533ce814ba9a27a4
jokokoloko <jhowland82@gmail.com>
2019-09-29 19:26:16 -0700

    Updated overview.js

--- pages/overview.js
+++ pages/overview.js
@@ -1,17 +1,44 @@
+import fetch from 'isomorphic-unfetch';
 import usePage from '../src/queries/usePage';
 import Layout from '../src/components/Layout';
-import Basic from '../src/components/section/Basic';
+import Feed from '../src/components/section/Feed';
+import NextLink from 'next/link'; // delete
 
-export default () => {
-    const { overview } = usePage();
+const Overview = ({ shows: posts }) => {
+    const { overview: page } = usePage();
+    const loopPost = posts.map((node) => (
+        <li key={node.id}>
+            <NextLink href="/post/[slug]" as={`/post/${node.id}`}>
+                <a>{node.name}</a>
+            </NextLink>
+        </li>
+    ));
     return (
-        <Layout template={`page page-${overview.slug}`} title={overview.title}>
-            <Basic id="basic-overview">
-                <header className="copy node-xs-50 node-lg-80 text-lg-center">
-                    <h1>{overview.title}</h1>
-                    <h2>{overview.description}</h2>
-                </header>
-            </Basic>
+        <Layout template={`page page-${page.slug}`} title={page.title} description={page.description}>
+            <Feed id={`feed-${page.slug}`} space="space-custom" item="post">
+                {(page.title || page.description) && (
+                    <header className="copy node-xs-50 node-lg-80 text-lg-center">
+                        <h1>{page.title}</h1>
+                        <h2>{page.description}</h2>
+                    </header>
+                )}
+                <section className="node-xs-50 node-lg-80 cheat-both">
+                    <div className="row gutter-20">
+                        <ul>{loopPost}</ul>
+                    </div>
+                </section>
+            </Feed>
         </Layout>
     );
 };
+
+Overview.getInitialProps = async () => {
+    const response = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+    const data = await response.json();
+    console.log(`Show data fetched. Count: ${data.length}`); // delete
+    return {
+        shows: data.map((entry) => entry.show),
+    };
+};
+
+export default Overview;
