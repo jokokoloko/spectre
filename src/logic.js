50bbaeae4d0b56034c8d8f89c2ad00742cb99bad
jokokoloko <jhowland82@gmail.com>
2019-12-20 21:38:17 -0800

    Updated logic.js

--- src/logic.js
+++ src/logic.js
@@ -1,2 +1,1 @@
-export const logicDescription = (type) =>
-    (type.excerpt && type.excerpt.excerpt) || (type.body && type.body.childMarkdownRemark.excerpt.replace(/\n/g, ' '));
+export const logicDescription = (type) => type.summary.replace(/(<p[^>]+?>|<p>|<\/p>)/gim, '');
